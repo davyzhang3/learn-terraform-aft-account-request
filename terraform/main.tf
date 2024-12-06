163e622bd1acc0f0e20bc5e83675ece9e1c6d5b9
davyzhang3 <davyzhang325@gmail.com>
2024-12-05 22:30:44 -0500

    feat: add sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
+# Gmail's plus addressing feature allows users to create unique email addresses by adding a "+" symbol and any word or characters after their username and before the "@" symbol. For example, if a user's email address is john@gmail.com, they can create john+shopping@gmail.com, john+work@gmail.com, or john+anythingyouwant@gmail.com. All of these email addresses will go to the user's inbox, but Gmail will treat them as separate email addresses
 module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "davyzhang325+sandbox@gmail.com" # The email will be sent to davyzhang325@gmail.com.
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "davyzhang325+sandbox@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
